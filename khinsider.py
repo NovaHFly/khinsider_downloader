8bdd9d778441788f91fc6ba723f750cccfc77af9
NovaHFly <mugiviska@gmail.com>
2025-01-22 19:54:26 +0300

    feat: Move track url collecting out of futures

--- khinsider.py
+++ khinsider.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 )
 logging.getLogger().addHandler(logging.StreamHandler())
 
-KHINSIDER_ALBUM_URL_REGEX = (
+KHINSIDER_URL_REGEX = (
     r'https:\/\/downloads\.khinsider\.com\/'
     r'game-soundtracks\/album\/([\w-]+)\/?([\w%.]+)?'
 )
@@ -25,7 +25,7 @@ KHINSIDER_BASE_URL = 'https://downloads.khinsider.com'
 
 DOWNLOADS_PATH = Path('downloads')
 
-THREAD_COUNT = 4
+THREAD_COUNT = 6
 
 
 @retry(stop=stop_after_attempt(5))
@@ -42,14 +42,34 @@ def read_links_from_file(file_path: str) -> list:
         return [line.strip() for line in f.readlines()]
 
 
+def scrape_album_track_urls(url: str) -> list[str]:
+    response = get_http(url)
+
+    soup = bs(response.text, 'lxml')
+    songlist_rows = soup.select_one('#songlist').select('tr')
+
+    return [
+        KHINSIDER_BASE_URL + anchor['href']
+        for row in songlist_rows
+        if (anchor := row.select_one('td a'))
+    ]
+
+
 class KhinsiderDownloader:
     def __init__(self):
         self.worker_limit = THREAD_COUNT
         self.executor = None
         self.tasks = []
 
-    def download_track(self, url: str, album_slug: str):
-        track_filename = unquote(unquote(url.rsplit('/', 1)[-1]))
+    def download_track(self, url: str):
+        match = re.match(KHINSIDER_URL_REGEX, url)
+
+        if not match:
+            logging.error(f'Invalid track link: {url}')
+            return
+
+        album_slug = match[1]
+        track_filename = unquote(unquote(match[2]))
         logging.info(f'Downloading track {track_filename} from {album_slug}')
 
         response = get_http(url)
@@ -65,42 +85,12 @@ class KhinsiderDownloader:
         with file_path.open('wb') as f:
             f.write(audio_response.content)
 
-    def download_from_link(self, url: str) -> None:
-        match = re.match(KHINSIDER_ALBUM_URL_REGEX, url)
-
-        if not match:
-            logging.error(f'Invalid link: {url}')
-            return
-
-        album_slug = match[1]
-
-        if match[2]:
-            self.download_track(url, album_slug)
-            return
-
-        # TODO: Collect tracks separately from futures
-        logging.info(f'Collecting tracks from {url}')
-        response = get_http(url)
-
-        soup = bs(response.text, 'lxml')
-        songlist_rows = soup.select_one('#songlist').select('tr')
-
-        for i, row in enumerate(songlist_rows):
-            anchor = row.select_one('td a')
-            if not anchor:
-                continue
-
-            track_url = KHINSIDER_BASE_URL + anchor['href']
-            logging.info(f'[{i}] Downloading {track_url}')
-            self.submit_download(track_url)
-
-        logging.info(f'All tracks submitted: {album_slug}')
-
     def __enter__(self):
         self.executor = ThreadPoolExecutor(max_workers=self.worker_limit)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        wait(self.tasks)
         self.executor.shutdown()
         self.executor = None
 
@@ -109,13 +99,7 @@ class KhinsiderDownloader:
             raise RuntimeError('Executor is not running')
 
         logging.info(f'{link} submitted for download!')
-        self.tasks.append(self.executor.submit(self.download_from_link, link))
-
-    def download(self, links: str):
-        for link in links:
-            self.submit_download(link)
-
-        wait(self.tasks)
+        self.tasks.append(self.executor.submit(self.download_track, link))
 
 
 def main() -> None:
@@ -128,10 +112,23 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    links = read_links_from_file(args.file)
+    links_from_file = read_links_from_file(args.file)
+    track_links = []
+
+    for link in links_from_file:
+        if not (match := re.match(KHINSIDER_URL_REGEX, link)):
+            logging.error(f'Invalid khinsider link: {link}')
+            continue
+
+        if match[2]:
+            track_links.append(link)
+            continue
+
+        track_links.extend(scrape_album_track_urls(link))
 
     with KhinsiderDownloader() as downloader:
-        downloader.download(links)
+        for link in track_links:
+            downloader.submit_download(link)
 
 
 if __name__ == '__main__':
