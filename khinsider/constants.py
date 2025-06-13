bae231ebdbf873c2279b9df0ea326ae6c05549df
NovaHFly <mugiviska@gmail.com>
2025-06-13 18:25:36 +0300

    Compile regex in constants.py

--- khinsider/constants.py
+++ khinsider/constants.py
@@ -1,10 +1,11 @@
+import re
 from pathlib import Path
 
 KHINSIDER_BASE_URL = 'https://downloads.khinsider.com'
 ALBUM_BASE_URL = f'{KHINSIDER_BASE_URL}/game-soundtracks/album'
 DOWNLOADS_PATH = Path('downloads')
 
-KHINSIDER_URL_REGEX = (
+KHINSIDER_URL_REGEX = re.compile(
     r'https:\/\/downloads\.khinsider\.com\/'
     r'game-soundtracks\/album\/([\w%.-]+)\/?([\w%.-]+)?'
 )
