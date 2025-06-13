b20b8fb264bdb8ca68b8e67824cda63925f8de3e
NovaHFly <mugiviska@gmail.com>
2025-06-13 13:22:45 +0300

    Make parse_khinsider_url raise exception when url is invalid

--- khinsider/util.py
+++ khinsider/util.py
@@ -3,24 +3,25 @@ from urllib.parse import quote, unquote
 
 from constants import KHINSIDER_URL_REGEX
 
+from .exceptions import InvalidUrl
+
 
 def normalize_query(query: str) -> str:
     full_quote = partial(quote, safe='')
     return '+'.join(map(full_quote, query.split()))
 
 
-def parse_khinsider_url(url: str) -> tuple[str | None, str | None]:
+def parse_khinsider_url(url: str) -> tuple[str, str | None]:
     """Extract album slug and track name from khinsider album-track url.
 
     Args:
         url (str): Valid khinsider url under /game-soundtracks/album/ path.
 
     Returns:
-        out (tuple[str|None, str|None]): album_slug, track_name;
-            all None if url is invalid.
+        out (tuple[str, str|None]): album_slug, track_name;
     """
     if not (match := KHINSIDER_URL_REGEX.match(url)):
-        return None, None
+        raise InvalidUrl(f'{url} is not an album or track url from khinsider')
 
     return match[1], match[2]
 
