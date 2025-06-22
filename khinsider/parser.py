f548f4b9cc06445673468a42e068d9fbecdef175
NovaHFly <mugiviska@gmail.com>
2025-06-22 17:32:31 +0300

    Add logging, docstrings and type casting to parser.py

--- khinsider/parser.py
+++ khinsider/parser.py
@@ -1,6 +1,5 @@
 import logging
 import re
-from typing import Any
 
 from bs4 import BeautifulSoup, Tag
 
@@ -11,45 +10,75 @@ logger = logging.getLogger('khinsider-parser')
 
 
 def parse_track_data(html_text: str) -> dict[str, str]:
+    """Parse track data from html."""
     match = re.search(r'<audio.+src="(.+)"', html_text)
 
     logger.debug(f'parse_track_data:{match = }')
 
     if not match:
+        logger.warning('Track data not found in html')
         return {}
 
     return {'mp3_url': match[1]}
 
 
-def parse_album_data(html_text: str) -> dict[str, Any]:
+def parse_album_data(html_text: str) -> dict[str, str | list[str] | None]:
+    """Parse album data from html."""
     soup = BeautifulSoup(html_text, 'lxml')
+
+    # * Title
+    title_tag = soup.select_one('h2')
+    if title_tag := soup.select_one('h2'):
+        album_title = title_tag.text
+    else:
+        logger.warning('Album title not found in html')
+        album_title = None
+
+    # * Art
+    album_art = [
+        anchor.attrs['href'] for anchor in soup.select('.albumImage a')
+    ]
+    if not album_art:
+        logger.warning('Album art not found in html')
+
+    # * Year
+    if match := re.search(r'Year: <b.(\d{4})</b>', html_text):
+        album_year: str = match[1]
+    else:
+        logger.warning('Album year not found in html')
+
+    # * Type
+    if tag := soup.select_one('p[align=left] b a'):
+        album_type = tag.text
+    else:
+        logger.warning('Album type not found in html')
+        album_type = None
+
+    # * Track page urls
+    track_urls = [
+        KHINSIDER_BASE_URL + anchor.attrs['href']
+        for row in soup.select('#songlist tr')
+        if (anchor := row.select_one('td a'))
+    ]
+    if not track_urls:
+        logger.warning('Album track urls not found in html')
+
     return {
-        'name': soup.select_one('h2').text,
-        'thumbnail_urls': [
-            anchor.attrs['href'] for anchor in soup.select('.albumImage a')
-        ],
-        'year': (
-            match[1]
-            if (match := re.search(r'Year: <b.(\d{4})</b>', html_text))
-            else None
-        ),
-        'type': (
-            tag.text if (tag := soup.select_one('p[align=left] b a')) else None
-        ),
-        'track_urls': [
-            KHINSIDER_BASE_URL + anchor['href']
-            for row in soup.select('#songlist tr')
-            if (anchor := row.select_one('td a'))
-        ],
+        'name': album_title,
+        'thumbnail_urls': album_art,
+        'year': album_year,
+        'type': album_type,
+        'track_urls': track_urls,
     }
 
 
 def parse_publisher_data(html_text: str) -> dict[str, str]:
+    """Parse publisher data from html."""
     match = re.search(r'Published by:.+<a href=".+/(.+)">(.+)</a>', html_text)
-
     logger.debug(f'parse_publisher_data:{match = }')
 
     if not match:
+        logger.warning('Publisher data not found in html')
         return {}
 
     return {
@@ -59,14 +88,24 @@ def parse_publisher_data(html_text: str) -> dict[str, str]:
 
 
 def parse_search_page(html_text: str) -> list[dict[str, str]]:
+    """Parse search results."""
+
     def _parse_table_row(row_tag: Tag) -> dict[str, str]:
         col_tags = row_tag.select('td')[1:]
 
-        name_anchor = col_tags[0].select_one('a')
+        if not col_tags or len(col_tags) < 4:
+            logger.warning('Unknown row markup')
+            return {}
+
+        if not (name_anchor := col_tags[0].select_one('a')):
+            logger.warning('No <a> tag found in search table row tag')
+            return {}
+
         album_name = name_anchor.text
 
-        album_url = KHINSIDER_BASE_URL + name_anchor.attrs['href']
-        album_slug = parse_khinsider_url(album_url)[0]
+        album_slug = parse_khinsider_url(
+            KHINSIDER_BASE_URL + name_anchor.attrs['href']
+        )[0]
 
         album_type = col_tags[2].text
         album_year = col_tags[3].text
@@ -81,6 +120,13 @@ def parse_search_page(html_text: str) -> list[dict[str, str]]:
     soup = BeautifulSoup(html_text, 'lxml')
 
     if not (result_tags := soup.select('table.albumList tr')[1:]):
+        logger.warning(
+            'Unknown search table format or search results not found in html'
+        )
         return []
 
-    return [_parse_table_row(tag) for tag in result_tags]
+    return [
+        parsed_row
+        for tag in result_tags
+        if (parsed_row := _parse_table_row(tag))
+    ]
