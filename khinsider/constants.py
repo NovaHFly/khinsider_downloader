837ee9bf8afb1e315d1af2a8e7d2a41903bddfcb
NovaHFly <mugiviska@gmail.com>
2025-06-22 17:37:37 +0300

    Add docstrings to constants

--- khinsider/constants.py
+++ khinsider/constants.py
@@ -2,12 +2,20 @@ import re
 from pathlib import Path
 
 KHINSIDER_BASE_URL = 'https://downloads.khinsider.com'
+"""Khinsider domain url with protocol and without trailing slash."""
 ALBUM_BASE_URL = f'{KHINSIDER_BASE_URL}/game-soundtracks/album'
+"""Base url for all khinsider album pages"""
 DOWNLOADS_PATH = Path('downloads')
+"""Default file downloads path"""
 
 KHINSIDER_URL_REGEX = re.compile(
     r'https:\/\/downloads\.khinsider\.com\/'
     r'game-soundtracks\/album\/([^\s/]+)\/?([^\s/]+)?'
 )
+"""
+Regex to check if khinsider url is valid
+and to extract album slug and track name.
+"""
 
 MAX_CONCURRENT_REQUESTS = 5
+"""Maximum count of concurrent requests to downloads.khinsider.com"""
